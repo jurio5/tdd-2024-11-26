a55623527773f6c4ee550472d7fba1e75c2376ba
jurio5 <sia5492@naver.com>
2024-11-26 11:35:00 +0900

    work

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.assertj:assertj-core:3.24.2") // AssertJ
+    testImplementation("org.assertj:assertj-core:3.24.2")
     testImplementation("org.hamcrest:hamcrest:2.2")
 }
 
--- src/test/java/com/ll/SimpleCalculatorTest.java
+++ src/test/java/com/ll/SimpleCalculatorTest.java
@@ -1,22 +1,88 @@
 package com.ll;
 
-import org.assertj.core.api.AbstractIntegerAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SimpleCalculatorTest {
 
     @Test
     @DisplayName("1 + 2 = 3")
-    public void testPlus() {
+    public void t1() {
         SimpleCalculator calculator = new SimpleCalculator();
 
         int rs = calculator.plus(1, 2);
 
-        assertEquals(3, rs);
+        assertThat(rs).isEqualTo(3);
+//        assertEquals(3, rs);
+    }
+
+    @Test
+    @DisplayName("2 + 10 = 12")
+    public void t2() {
+        SimpleCalculator calculator = new SimpleCalculator();
+
+        int rs = calculator.plus(2, 10);
+
+        assertThat(rs).isEqualTo(12);
+//        assertEquals(12, rs);
+    }
+
+    @Test
+    @DisplayName("20 + 10 = 30")
+    public void t3() {
+        SimpleCalculator calculator = new SimpleCalculator();
+
+        int rs = calculator.plus(20, 10);
+
+        assertThat(rs).isEqualTo(30);
+//        assertEquals(30, rs);
+    }
+
+    @Test
+    @DisplayName("2 * 3 = 6")
+    public void multiply1() {
+        SimpleCalculator calculator = new SimpleCalculator();
+
+        int rs = calculator.multiply(2, 3);
+
+        assertThat(rs).isEqualTo(6);
+//        assertEquals(6, rs);
+    }
+
+    @Test
+    @DisplayName("3 * 2 = 1")
+    public void minus1() {
+        SimpleCalculator calculator = new SimpleCalculator();
+
+        int rs = calculator.minus(3, 2);
+
+        assertThat(rs).isEqualTo(1);
+//        assertEquals(1, rs);
+    }
+
+    @Test
+    @DisplayName("5 / 2 = 2")
+    public void divide1() {
+        SimpleCalculator calculator = new SimpleCalculator();
+
+        int rs = calculator.divide(5, 2);
+
+        assertThat(rs).isEqualTo(2);
+//        assertEquals(2, rs);
+    }
+
+    @Test
+    @DisplayName("3 % 2 = 1 .. 1")
+
+    public void reminder1() {
+        SimpleCalculator calculator = new SimpleCalculator();
+
+        int rs = calculator.reminder(3, 2);
+
+        assertThat(rs).isEqualTo(1);
+//        assertEquals(1, rs);
     }
 }
